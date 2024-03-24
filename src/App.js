9ee73c79beefe912529ae639d2c50cf5e17cb7ee
Jaclyn Sunardi <jaclyn.sunardi@yahoo.ca>
2024-03-23 19:12:15 -0700

    new pages created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 
-
-
 function App() {
   return (
     <div className="App">
       <div className = "Loading_Screen">
         <img src = "Logo.png" alt = "Logo"/>
-
       </div>
     </div>
   );
