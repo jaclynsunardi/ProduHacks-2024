0ecf13e3c28b060f4fa42c02640db90bbd38c8c9
Jaclyn Sunardi <jaclyn.sunardi@yahoo.ca>
2024-03-23 19:29:33 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
+import { Routes } from './Routes';
 import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
-      <div className = "Loading_Screen">
-        <img src = "Logo.png" alt = "Logo"/>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
---
+++ src/pages/loadingScreen.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+export const loadingScreen = () => {
+    return (
+        <img src = "Logo.png" alt = "Logo"/>
+    );
+}
