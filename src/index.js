984d89acf85de050cae72c75485b2f0322cb496b
safiullahkaleem2 <safiullahkaleem867@gmail.com>
2024-03-24 09:57:14 -0700

    login screen

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import LoadingScreen from './pages/loadingScreen';
 import HomeScreen from './pages/homeScreen';
-import loginScreen from './pages/loginScreen';
+import LoginScreen from './pages/loginScreen';
+import { ClerkProvider } from '@clerk/clerk-react'
+import ProgressScreen from './pages/progressScreen';
+const PUBLISHABLE_KEY = "pk_test_dW5jb21tb24tdG9ydG9pc2UtNjguY2xlcmsuYWNjb3VudHMuZGV2JA";
+ 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+ 
 
 const router = createBrowserRouter([
   {
@@ -21,16 +29,23 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <HomeScreen />,
+    element: <LoginScreen />,
+  },
+  {
+    path: "/progess",
+    element: <ProgressScreen />,
   },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <RouterProvider router={router} />
+  </ClerkProvider>
+</React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
