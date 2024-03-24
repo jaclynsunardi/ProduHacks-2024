42c990508afb796ce47b411022f9865a0cdf50ca
Jaclyn Sunardi <jaclyn.sunardi@yahoo.ca>
2024-03-23 19:38:48 -0700

    Revert "temp"
    This reverts commit 7f8972ba17a5edf24f9ab50ac4e32959f9cc4264.

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { loadingScreen } from './pages/loadingScreen';
 import { loginScreen } from './pages/loadingScreen';
-import { homeScreen } from './pages/homeScreen';
 
 export const Routes = () => {
     return (
         <Router>
-            <Route path="/">
-                <loadingScreen />
-            </Route>
-            <Route path = "/screen/:screen1 ">
-                <loginScreen />
-            </Route>
+            <Switch>
+                <Route path="/">
+                    <loadingScreen />
+                </Route>
+                <Route path = "/screen/:screen1 ">
+                    <loginScreen />
+                </Route>
+            </Switch>
         </Router>
     )
 }
--- src/pages/loadingScreen.js
+++ src/pages/loadingScreen.js
@@ -1,4 +1,4 @@
-import '../App.css';
+import './App.css';
 
 export const loadingScreen = () => {
     return (
