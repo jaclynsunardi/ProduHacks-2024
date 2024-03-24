c2913c83674d82ae14485a8a6a0f635f66fc51ad
nrhuang <nhuangra@gmail.com>
2024-03-23 21:37:28 -0700

    added addactivity header

--- src/pages/addActivityScreen.js
+++ src/pages/addActivityScreen.js
@@ -1,14 +1,22 @@
 import '../css/global.css';
+import '../css/addActivity.css';
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 export default function AddActivityScreen () {
-    return (
+  let navigate = useNavigate();
+  const toHome = () => {
+    let path = `/home`;
+    navigate(path);
+  }
+  
+  return (
       <div className = "AddActivityScreen">
-        <nav>
-          <p>Back</p>
-          <h1>Add Activity</h1>
-          <p>Publish</p>
-          
-        </nav>
+        <div className="add-activity-header">
+          <button onClick={toHome} className='header-back-button'>Back</button>
+          <button onClick={toHome} className='header-publish-button'>Publish</button>
+          <h1 className='header-title'>Add Activity</h1>
+        </div>
       </div>
     );
   }
